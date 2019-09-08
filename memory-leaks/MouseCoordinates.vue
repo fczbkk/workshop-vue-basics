3ab98bf4534fa15453239735178abed45204cfb0
Riki Fridrich <riki@fczbkk.com>
2019-09-08 07:31:14 +0200

    add memory leak example

---
+++ memory-leaks/MouseCoordinates.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Mouse coordinates</h1>
+    <p>Left: {{ left }}</p>
+    <p>Top: {{ top }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        left: 0,
+        top: 0
+      }
+    },
+    methods: {
+      handleMouseMove (event) {
+        this.left = event.clientX
+        this.top = event.clientY
+      }
+    },
+    mounted () {
+      window.addEventListener('mousemove', this.handleMouseMove)
+    },
+    destroyed () {
+      window.removeEventListener('mousemove', this.handleMouseMove)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ memory-leaks/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>
+      <label>
+        <input type="checkbox" v-model="showMouseCoordinates">
+        Show mouse coordinates
+      </label>
+    </p>
+    <mouse-coordinates v-if="showMouseCoordinates" />
+  </div>
+</template>
+
+<script>
+  import MouseCoordinates from './MouseCoordinates.vue'
+
+  export default {
+    components: {
+      MouseCoordinates
+    },
+    data () {
+      return {
+        showMouseCoordinates: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
