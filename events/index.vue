d36225019a552462e3f8e33b1734b584320bfbd8
Riki Fridrich <riki@fczbkk.com>
2019-09-08 06:44:43 +0200

    add events example

---
+++ events/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>
+      <button @click="clicksCounter += 1">
+        Click this button
+      </button>
+    </p>
+    <p>Button has been clicked {{ clicksCounter }} times.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clicksCounter: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
