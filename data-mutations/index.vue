c54860a664bae7d8daa8c031eedc65aa8cbd3f04
Riki Fridrich <riki@fczbkk.com>
2019-09-08 06:09:58 +0200

    add data mutation sample

---
+++ data-mutations/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <h1>Personal data</h1>
+    <pre>{{ person }}</pre>
+
+    <h1>Direct object manipulation</h1>
+    <p>
+      <button @click="person.lastName = 'Fridrich'">Add last name</button>
+      <button @click="person.nick = 'Fczbkk'">Add nick</button>
+    </p>
+
+    <h1>Object mutation</h1>
+    <p>
+      <button @click="addLastName">Add last name</button>
+      <button @click="addNick">Add nick</button>
+    </p>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        person: {
+          firstName: 'Riki'
+        }
+      }
+    },
+    methods: {
+      addLastName () {
+        this.$set(this.person, 'lastName', 'Fridrich')
+      },
+      addNick () {
+        this.$set(this.person, 'nick', 'Fczbkk')
+      }
+    }
+  }
+</script>
