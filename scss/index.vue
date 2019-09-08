9cdecb3d331199783e603300d7bfc8bfb4834a34
Riki Fridrich <riki@fczbkk.com>
2019-09-08 06:00:16 +0200

    add SCSS example

---
+++ scss/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <h1>Title using primary color</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  $primary-color: #c00;
+
+  // nesting
+  .wrapper {
+
+    h1 {
+      // variable
+      color: $primary-color;
+    }
+
+  }
+
+</style>
