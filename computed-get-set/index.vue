f7158b75952cdc3bb9a998ec8c02dea0a39a19ba
fczbkk <riki@fczbkk.com>
2019-11-04 13:54:42 +0100

    add computed getter / setter example

---
+++ computed-get-set/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>First:<br><input v-model="firstName" /></p>
+    <p>Last:<br><input v-model="lastName" /></p>
+    <p>Full:<br><input v-model="fullName" /></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        firstName: 'John',
+        lastName: 'Doe'
+      }
+    },
+    computed: {
+      fullName: {
+        get () {
+          return [this.firstName, this.lastName]
+            .filter((chunk) => chunk !== '')
+            .join(' ')
+        },
+        set (newName) {
+          const [first = '', last = ''] = newName.split(' ')
+          this.firstName = first
+          this.lastName = last
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
