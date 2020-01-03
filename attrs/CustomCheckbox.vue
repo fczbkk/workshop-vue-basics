2b1ad0b42b853d79deb73c9e43fdfb0f81e4c49c
fczbkk <riki@fczbkk.com>
2020-01-03 10:04:46 +0100

    add $attrs example

---
+++ attrs/CustomCheckbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <label>
+    <input
+      type="checkbox"
+      :checked="value"
+      @change="$emit('input', !value)"
+      v-bind="$attrs"
+    />
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'CustomCheckbox',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ attrs/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>
+      <custom-checkbox>Simple example</custom-checkbox>
+    </p>
+    <p>
+      <custom-checkbox :disabled="true">Disabled</custom-checkbox>
+    </p>
+  </div>
+</template>
+
+<script>
+import CustomCheckbox from './CustomCheckbox'
+export default {
+  components: { CustomCheckbox }
+}
+</script>
+
+<style scoped>
+
+</style>
