5bbf97c766f056f8357674fe7cf9940da763081b
fczbkk <riki@fczbkk.com>
2020-01-07 12:54:59 +0100

    add watch example

---
+++ watch/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ randomValue }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      randomValue: 0
+    }
+  },
+  mounted () {
+    // update to a random value from 0 to 100 every second
+    setInterval(() => {this.randomValue = Math.round(Math.random() * 100)}, 1000)
+  },
+  watch: {
+    randomValue (newValue, oldValue) {
+      // eslint-disable-next-line
+      console.log(`Random value changed from ${oldValue} to ${newValue}.`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
